e4d1388e70feac14788a0e534e930f526403badc
wittawat <notzalnwza@hotmail.com>
2019-04-24 15:38:47 +0700

    info2 ui complete

--- src/main/java/sdu/cs/wittawat/universityinfo/MainActivity.java
+++ src/main/java/sdu/cs/wittawat/universityinfo/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
             {
                     R.drawable.logo1, R.drawable.logo2, R.drawable.logo3,
                     R.drawable.logo4, R.drawable.logo5, R.drawable.logo6,
-                    R.drawable.logo7, R.drawable.logo8, R.drawable.logo9,   };
+                    R.drawable.logo7, R.drawable.logo8, R.drawable.logo9,       };
     String[] detailString, approach1, approach2, approach3, driveString, responsibleString;
     Image aboutApp;
 
@@ -30,11 +30,11 @@ public class MainActivity extends AppCompatActivity {
 
         //ดึงค่าจากmy contentมาลงxml
         detailString = getResources().getStringArray(R.array.detail);
-        approach1 = getResources().getStringArray(R.array.approach1);
+        /* approach1 = getResources().getStringArray(R.array.approach1);
         approach2 = getResources().getStringArray(R.array.approach2);
         approach3 = getResources().getStringArray(R.array.approach3);
         driveString = getResources().getStringArray(R.array.drive);
-        responsibleString = getResources().getStringArray(R.array.responsible);
+        responsibleString = getResources().getStringArray(R.array.responsible); */
 
         //create listview
         MyAdapter myAdapter = new MyAdapter(MainActivity.this, ints);
@@ -49,9 +49,9 @@ public class MainActivity extends AppCompatActivity {
                 //ส่งdata
                 infoIntent.putExtra("Logo", ints[i]);
                 infoIntent.putExtra("Detail", detailString[i]);
-                infoIntent.putExtra("Approach1", approach1[i]);
+                /* infoIntent.putExtra("Approach1", approach1[i]);
                 infoIntent.putExtra("Approach2", approach2[i]);
-                infoIntent.putExtra("Approach3", approach3[i]);
+                infoIntent.putExtra("Approach3", approach3[i]); */
                 //เริ่มส่ง
                 startActivity(infoIntent);
             }
--- src/main/java/sdu/cs/wittawat/universityinfo/info2.java
+++ src/main/java/sdu/cs/wittawat/universityinfo/info2.java
@@ -2,12 +2,36 @@ package sdu.cs.wittawat.universityinfo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class info2 extends AppCompatActivity {
+
+    //explicit
+    TextView data1,data2, data3;
+    String dv1,dv2,dv3,dv4,rp1,rp2,rp3, rp4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info2);
-    }
-}
+
+        //เชื่อมxml
+        data1 = findViewById(R.id.data1txt);
+        data2 = findViewById(R.id.data2txt);
+        data3 = findViewById(R.id.data3txt);
+
+        //รับค่าจากintentมาแสดง
+        data1.setText(getIntent().getStringExtra("Appro1"));
+        data2.setText(getIntent().getStringExtra("Appro2"));
+        data3.setText(getIntent().getStringExtra("Appro3"));
+
+
+
+
+    } //onCreate
+
+    public void backButton(View view) {
+        finish();
+    } // backButton
+} // class
